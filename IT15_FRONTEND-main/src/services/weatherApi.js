095794a65f1333f633662fd6a7630b19a659c6de
Mary Grace Bautista <m.bautista.143372.tc@umindanao.edu.ph>
2026-03-13 22:18:58 +0800

    Delete IT15_FRONTEND-main directory

--- IT15_FRONTEND-main/src/services/weatherApi.js
+++
@@ -1,4 +0,0 @@
-import api from "./api";
-
-export const fetchCurrentWeather = (params) => api.get("/weather/current", { params });
-export const fetchForecast = (params) => api.get("/weather/forecast", { params });
